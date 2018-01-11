02947b4f1fee4d062d1dc42dfba1325792196c93
Pierre Mareschal <pierre.mareschal@oscars-sa.eu>
2018-01-11 18:16:10 +0100

    working for countup

--- js/storyrevealer-plugin.js
+++ js/storyrevealer-plugin.js
@@ -7,15 +7,18 @@
 
 var RevealJSAnimation = window.RevealJSAnimation || (function(){
 	var _animations = {}
+	var _id_counter = 0
 	
 	return {
 		register: function(name, anim) {
 			_animations[name] = anim
+			console.log("RevealJSAnimation::register", name)
 			return anim
 		},
 		play: function(name) {
 			if(_animations[name]) {
 				_animations[name].play()
+				console.log("RevealJSAnimation::play", name)
 			}
 		},
 		pause: function(name) {
@@ -32,40 +35,77 @@ var RevealJSAnimation = window.RevealJSAnimation || (function(){
 			if(_animations[name]) {
 				_animations[name].reverse()
 			}
+		},
+		generateId: function() {
+			return 'a'+(_id_counter++)
+		},
+		play_animations: function(container) {
+			if(container.getAttribute('data-animation')) {
+				var id = container.getAttribute("id")
+				RevealJSAnimation.restart(id)
+				console.log("RevealJSAnimation::play_animation::container", id)
+			}
+			var animations = container.querySelectorAll("*[data-animation]")
+			for (var i = 0; i < animations.length; i++ ) {
+				var animation = animations[i]
+				var id = animation.getAttribute("id")
+				RevealJSAnimation.restart(id)
+				console.log("RevealJSAnimation::play_animations", id)
+			}
 		}
 	}
 })()	
 
-function do_countup(container) {
-	var counters = container.querySelectorAll("*[data-countup]")
-	for (var i = 0; i < counters.length; i++ ) {
-		console.log(counters[i])
-		var values = counters[i].dataset.countup.split(',')
-		var myObject = {
-			counter: values[0]
+// Install all animations without starting them
+Reveal.addEventListener( 'ready', function( event ) {
+	var animations = document.querySelectorAll("*[data-animation]")
+	for (var i = 0; i < animations.length; i++ ) {
+		var animation = animations[i]
+		var animation_type = animation.getAttribute("data-animation")
+		var id = animation.getAttribute("id")
+		if(! id) {
+			id = RevealJSAnimation.generateId()
+			animation.setAttribute("id", id)
+			console.log('generated id', animation_type, id)
 		}
-		var cssProperties = anime({
-			targets: myObject,
-			counter: values[1],
-			round: values[2],
-			easing: 'easeInOutQuad',
-			duration: values[3],
-			update: function() {
-				 counters[i].innerHTML = myObject.counter;
-			}
-		})
-		console.log('anim added', 'countup')
-	}
-}
+		switch(animation_type) {
+			case "countup": (function() { // start each anim in its own context
+				var values = animation.dataset.countup.split(',')
+				var myObject = {
+					target: animation,
+					counter: values[0]
+				}
+				RevealJSAnimation.register(id, anime({
+					targets: myObject,
+					counter: values[1],
+					round: values[2],
+					easing: 'easeInOutQuad',
+					duration: values[3],
+					autoplay: false,
+					update: function() {
+						 myObject.target.innerHTML = myObject.counter;
+					}
+				}))
+				console.log('anim added', 'countup', id)
 
-Reveal.addEventListener( 'fragmentshown', function( event ) {
-	do_countup(event.fragment)
+				})()
+				break
+			case "skill-cursor":
+			case "moving-letters":
+			default:
+				console.log('ready::install animations::animation type not found', animation)
+				break
+		}
+	}
 } );
 
 Reveal.addEventListener( 'slidechanged', function( event ) {
-	do_countup(Reveal.getCurrentSlide())
+	RevealJSAnimation.play_animations(Reveal.getCurrentSlide())
 } );
 
+Reveal.addEventListener( 'fragmentshown', function( event ) {
+	RevealJSAnimation.play_animations(event.fragment)
+} );
 
 
 function do_skill_table(container, enable) {

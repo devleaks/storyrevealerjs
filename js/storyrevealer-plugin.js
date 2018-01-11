5d6bcbdba8074434f1173e7871df7c3ffa8be0c4
Pierre Mareschal <pierre.mareschal@oscars-sa.eu>
2018-01-11 19:15:47 +0100

    working for countup and progress-bar

--- js/storyrevealer-plugin.js
+++ js/storyrevealer-plugin.js
@@ -8,6 +8,34 @@
 var RevealJSAnimation = window.RevealJSAnimation || (function(){
 	var _animations = {}
 	var _id_counter = 0
+	var _colors = [
+		"#1abc9c",
+		"#2ecc71",
+		"#3498db",
+		"#9b59b6",
+		"#34495e",
+		"#16a085",
+		"#27ae60",
+		"#2980b9",
+		"#8e44ad",
+		"#2c3e50",
+		"#f1c40f",
+		"#e67e22",
+		"#e74c3c",
+		"#ecf0f1",
+		"#95a5a6",
+		"#f39c12",
+		"#d35400",
+		"#c0392b",
+		"#bdc3c7",
+		"#7f8c8d"
+	]
+	var _templates = {
+		"countup": "",
+		"countup-table": "<table class='counter-table'>{{#skills}}<tr><td>{{name}}</td><td><span data-animation='countup' data-countup='0,{{value}},1,2000'>{{value}}</span></td></tr>{{/skills}}</table>",
+		"progress-bar": "<div data-animation='progress-bar' data-progress-bar-name='{{name}}' data-progress-bar-value='{{value}}'><div class='progress-bar-name'>{{name}}</div>{{#description}}<div class='progress-bar-desc'>{{description}}</div>{{/description}}<span class='progress-bar'></span></div>",
+		"progress-bar-table": "<div class='progress-bar-table'>{{#skills}}<div class='right' data-animation='progress-bar' data-progress-bar-name='{{name}}' data-progress-bar-value='{{value}}'><div class='progress-bar-name'>{{name}}</div>{{#description}}<div class='progress-bar-desc'>{{description}}</div>{{/description}}<span class='progress-bar'></span></div>{{/skills}}</div>",	
+	}
 	
 	return {
 		register: function(name, anim) {
@@ -36,15 +64,25 @@ var RevealJSAnimation = window.RevealJSAnimation || (function(){
 				_animations[name].reverse()
 			}
 		},
+		id: function() {
+			return _id_counter
+		},
 		generateId: function() {
 			return 'a'+(_id_counter++)
 		},
+		color: function() {
+			return _colors[_id_counter % _colors.length]
+		},
 		play_animations: function(container) {
-			if(container.getAttribute('data-animation')) {
+			if(container.getAttribute('data-animation')) { // play animation on current element, if any
 				var id = container.getAttribute("id")
 				RevealJSAnimation.restart(id)
 				console.log("RevealJSAnimation::play_animation::container", id)
 			}
+			/*	The following selector IS NOT CORRECT. we should only search for animation but without .fragment style
+				correct algorithm need to bring in jquery (See: https://github.com/hakimel/reveal.js/issues/833)
+				so we'll stick to this for now
+			*/
 			var animations = container.querySelectorAll("*[data-animation]")
 			for (var i = 0; i < animations.length; i++ ) {
 				var animation = animations[i]
@@ -57,7 +95,7 @@ var RevealJSAnimation = window.RevealJSAnimation || (function(){
 })()	
 
 // Install all animations without starting them
-Reveal.addEventListener( 'ready', function( event ) {
+Reveal.addEventListener( 'ready' , function( event ) {
 	var animations = document.querySelectorAll("*[data-animation]")
 	for (var i = 0; i < animations.length; i++ ) {
 		var animation = animations[i]
@@ -68,7 +106,9 @@ Reveal.addEventListener( 'ready', function( event ) {
 			animation.setAttribute("id", id)
 			console.log('generated id', animation_type, id)
 		}
+		
 		switch(animation_type) {
+
 			case "countup": (function() { // start each anim in its own context
 				var values = animation.dataset.countup.split(',')
 				var myObject = {
@@ -90,7 +130,22 @@ Reveal.addEventListener( 'ready', function( event ) {
 
 				})()
 				break
-			case "skill-cursor":
+
+			case "progress-bar": (function() { 
+				RevealJSAnimation.register(id, anime({
+				  targets: '#'+id+" span.progress-bar",
+				  width: animation.dataset['progress-bar-value']+'%',
+				  'background-color': RevealJSAnimation.color(),
+				  opacity: 1,
+				  easing: 'easeInOutQuad',
+				  autoplay: false,
+				  duration: 3000
+				}))
+				console.log('anim added', 'progress-bar', id)
+
+				})()
+				break
+
 			case "moving-letters":
 			default:
 				console.log('ready::install animations::animation type not found', animation)
@@ -99,53 +154,14 @@ Reveal.addEventListener( 'ready', function( event ) {
 	}
 } );
 
-Reveal.addEventListener( 'slidechanged', function( event ) {
+// Play animations on new slide
+Reveal.addEventListener( 'slidechanged' , function( event ) {
+	console.log('slidechanged::play_animations')	
 	RevealJSAnimation.play_animations(Reveal.getCurrentSlide())
 } );
 
-Reveal.addEventListener( 'fragmentshown', function( event ) {
+// Play animations on new fragment shown
+Reveal.addEventListener( 'fragmentshown' , function( event ) {
+	console.log('fragmentshown::play_animations')	
 	RevealJSAnimation.play_animations(event.fragment)
-} );
-
-
-function do_skill_table(container, enable) {
-	var colors = [ '#e00', '#0e0', '#00e' , '#ee0', '#e0e', '#0ee' ]
-	var skillbars = container.querySelectorAll("div[data-skillname]")
-	for (var i = 0; i < skillbars.length; i++ ){
-		var skillName = skillbars[i].dataset.skillname
-		var selector = '.skill[data-skillname="'+skillName+'"] span'
-		if(enable) {
-			var cssProperties = anime({
-			  targets: selector,
-			  width: skillbars[i].dataset.skillvalue+'%',
-			  'background-color': colors[i % colors.length],
-			  opacity: 1,
-			  easing: 'easeInOutQuad',
-			  duration: 3000
-			});
-			console.log('anim added', 'skill-cursor', selector, skillbars[i].dataset.skillvalue)
-		} else {
-			var cssProperties = anime({
-			  targets: selector,
-			  width: skillbars[i].dataset.skillvalue+'%',
-			  'background-color': colors[i % colors.length],
-			  opacity: 0,
-			  easing: 'easeInOutQuad',
-			  duration: 3000
-			});
-			console.log('anim added', 'skill-cursor', selector, skillbars[i].dataset.skillvalue)
-		}
-	}
-}
-
-Reveal.addEventListener( 'slidechanged', function( event ) {
-	do_skill_table(Reveal.getCurrentSlide(), true)
-} );
-
-Reveal.addEventListener( 'fragmentshown', function( event ) {
-	do_skill_table(event.fragment, true)
-} );
-
-Reveal.addEventListener( 'fragmenthidden', function( event ) {
-	do_skill_table(event.fragment, false)
 } );
